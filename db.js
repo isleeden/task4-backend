b70e68a56d49d3cf381a0f192bfbc7da89f09ada
isleeden <nkoreanarmy@gmail.com>
2022-06-03 02:04:46 +0500

    replaceb bd

--- db.js
+++ db.js
@@ -1,13 +1,4 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    port: process.env.DB_PORT,
-  }
-);
+module.exports = new Sequelize(process.env.DB_URI);
